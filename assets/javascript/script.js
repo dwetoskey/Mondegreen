504243431ae310fc2da86d134b54b4485dde9381
dwetoskey <dwetoskey224@gmail.com>
2022-01-13 14:06:05 -0500

    added function to pull form info into url, changed html button to js button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,26 @@
-var requestURL = "https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=love&page_size=3&page=1&apikey=bd6abc7893804c49fc007988f1aa727f"
+var requestURL = "https://musicbrainz.org/ws/2/recording?query=";
+
+let btn = document.createElement("button");
+btn.innerHTML = "Search";
+document.body.appendChild(btn);
 
 
-fetch(requestURL)
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data)
-});
+btn.onclick = function() {
+    var lyricquery = document.getElementById('lyricinput')
+    var finalURL = "https://musicbrainz.org/ws/2/recording?query=" + lyricquery;
+    console.log(finalURL);
+
+}
+
+// fetch(requestURL)
+// .then(function(response){
+//     console.log(response.json)
+//     return response.json();
+// })
+// .then(function(data){
+//     console.log(data)
+// });
+
+
+
+console.log("test");
